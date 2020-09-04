488ab00c3a74e73249ede0d963f18426e5171776
Sagar Kaushal <sagar.kaushal@northcorp.tech>
2020-09-04 12:43:30 +0530

    first commit

---
+++ src/main/java/org/vecv/demo/Hello.java
@@ -0,0 +1,15 @@
+package org.vecv.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+	
+	@RequestMapping("/")
+	public String demo()
+	{
+	return "This is a Rest Service";
+	}
+}
---
+++ src/test/java/org/vecv/demo/VecvdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vecv.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VecvdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
